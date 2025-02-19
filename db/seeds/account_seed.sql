15c8ab216d88db8c33fd578a0bf575e1025e0402
desmondfowler <desmondfowler14@gmail.com>
2025-02-19 11:03:30 -0600

    Update account_seed.sql to change 'role' to 'user_role'

--- db/seeds/account_seed.sql
+++ db/seeds/account_seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO accounts (username, email, hashed_password, first_name, last_name, role)
+INSERT INTO accounts (username, email, hashed_password, first_name, last_name, user_role)
 VALUES 
   ('Deleted-Account', 'bha@example.com', 'hashed_pw0', 'Deleted', 'Account', 'admin'),
   ('bigbootylover69420', 'bigbootylover69420@example.com', 'hashed_pw_1', 'Aranarion', 'ThErAnGeR', 'admin'),
