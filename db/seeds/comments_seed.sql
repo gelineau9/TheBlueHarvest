12ff68dd3a1101ff4f6a5dd830d55ed9933214a9
desmondfowler <desmondfowler14@gmail.com>
2025-02-19 11:20:59 -0600

    Fix seed files for accounts, characters, relationships, objects, posts, comments, and media

--- db/seeds/comments_seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO comments (post_id, account_id, comment)
-VALUES 
-  (1, 2, 'Welcome to Bree, traveler. Keep an eye out for brigands!'),
-  (2, 1, 'A fine axe indeed. May it serve you well in battle.'),
-  (3, 1, 'Those orcs won’t know what hit them!');
