b745a9d1a74e0a93f5c1cb02f9327a2beccd2a2f
desmondfowler <desmondfowler14@gmail.com>
2025-02-19 11:08:05 -0600

    Update character_relationships_seed.sql to change 'rival' to 'enemy'

--- db/seeds/char_relations_seed.sql
+++ db/seeds/char_relations_seed.sql
@@ -2,4 +2,4 @@ INSERT INTO character_relationships (character_id, related_character_id, relatio
 VALUES 
   (1, 2, 'friend'),   -- Faenor and Thrandor are friends
   (1, 3, 'ally'),     -- Faenor and Sarya are allies
-  (2, 3, 'rival');    -- Thrandor and Sarya are rivals
+  (2, 3, 'enemy');    -- Thrandor and Sarya are enemies
