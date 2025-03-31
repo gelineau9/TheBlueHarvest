35dd6a6a5c9650d367236899999b58c49ba8aea3
desmondfowler <desmondfowler14@gmail.com>
2025-03-31 11:44:36 -0500

    Removed subquery from constraint and used new type tables instead

--- db/schema/005_constraints.sql
+++ db/schema/005_constraints.sql
@@ -20,15 +20,26 @@ ADD CONSTRAINT profile_id_order CHECK (profile_id_1 < profile_id_2);
 -- bidirectional_relationships Table
 ALTER TABLE bidirectional_relationships
 ADD CONSTRAINT direction_both CHECK (direction = 'both'),
-ADD CONSTRAINT bidirectional_type CHECK (
-    EXISTS (SELECT 1 FROM relationship_types rt WHERE rt.type_id = bidirectional_relationships.type_id AND rt.is_bidirectional = TRUE)
-),
 ADD CONSTRAINT unique_bidirectional UNIQUE (profile_id_1, profile_id_2, type_id);
 
 -- unidirectional_relationships Table
 ALTER TABLE unidirectional_relationships
 ADD CONSTRAINT direction_single CHECK (direction IN ('forward', 'backward')),
-ADD CONSTRAINT unidirectional_type CHECK (
-    EXISTS (SELECT 1 FROM relationship_types rt WHERE rt.type_id = unidirectional_relationships.type_id AND rt.is_bidirectional = FALSE)
-),
 ADD CONSTRAINT unique_unidirectional UNIQUE (profile_id_1, profile_id_2, type_id, direction);
+
+
+-- -- bidirectional_relationships Table
+-- ALTER TABLE bidirectional_relationships
+-- ADD CONSTRAINT direction_both CHECK (direction = 'both'),
+-- ADD CONSTRAINT bidirectional_type CHECK (
+--     EXISTS (SELECT 1 FROM relationship_types rt WHERE rt.type_id = bidirectional_relationships.type_id AND rt.is_bidirectional = TRUE)
+-- ),
+-- ADD CONSTRAINT unique_bidirectional UNIQUE (profile_id_1, profile_id_2, type_id);
+
+-- -- unidirectional_relationships Table
+-- ALTER TABLE unidirectional_relationships
+-- ADD CONSTRAINT direction_single CHECK (direction IN ('forward', 'backward')),
+-- ADD CONSTRAINT unidirectional_type CHECK (
+--     EXISTS (SELECT 1 FROM relationship_types rt WHERE rt.type_id = unidirectional_relationships.type_id AND rt.is_bidirectional = FALSE)
+-- ),
+-- ADD CONSTRAINT unique_unidirectional UNIQUE (profile_id_1, profile_id_2, type_id, direction);
